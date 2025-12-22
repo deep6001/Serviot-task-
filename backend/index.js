fb5409c51299066327edd59524d33044116a0fe4
Deep Patel <pateldeep120404@gmail.com>
2025-12-23 00:15:28 +0530

    fix cors error

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173' || process.env.CLIENT_URL,
+  origin: 'https://serviot-task.vercel.app' || process.env.CLIENT_URL,
   credentials: true
 }));
 connectDB();
